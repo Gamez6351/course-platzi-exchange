098389c41c82ed29f9f398a09295dcab63f655e9
Carlos Gámez <juankgl11@hotmail.com>
2023-02-24 01:46:01 -0600

    Project end

---
+++ src/views/Error.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex-col items-center mt-32">
+    <h1 class="text-gray-700 text-6xl uppercase">404</h1>
+    <p>La página que buscas no ha sido encontrada!</p>
+    <router-link class="mt-5 text-xl text-green-600 hover:underline" to="/"
+      >Volver a la página de Inicio</router-link
+    >
+  </div>
+</template>
